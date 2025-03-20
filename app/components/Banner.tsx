d09d361fb61e89fb0f54faeae9e4391142038919
ratul0407 <rajaulislamratul12@gmail.com>
2025-03-20 16:19:52 +0600

    first commit

---
+++ app/components/Banner.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Navbar } from "./Navbar";
+
+function Banner() {
+  return (
+    <div className="bg-banner min-h-screen bg-cover text-white">
+      <Navbar />
+    </div>
+  );
+}
+
+export default Banner;
