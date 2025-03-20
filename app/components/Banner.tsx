a2716cf82f5f13f18fef4eeadc14d83f58dce68c
ratul0407 <rajaulislamratul12@gmail.com>
2025-03-20 16:40:33 +0600

    completed navbar for desktop

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -3,7 +3,7 @@ import { Navbar } from "./Navbar";
 
 function Banner() {
   return (
-    <div className="bg-banner min-h-screen bg-cover text-white">
+    <div className="bg-banner min-h-screen  bg-cover text-white">
       <Navbar />
     </div>
   );
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,3 +1,20 @@
+import { IoMenu } from "react-icons/io5";
+
 export const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="p-6">
+      <header className="flex justify-between">
+        {/* menu button */}
+        <button className="flex items-center gap-2">
+          <IoMenu className="h-6 w-6" />
+          <span className="text-lg">Menu</span>
+        </button>
+        {/* heading Logo */}
+        <h3 className="font-satisfy text-3xl">Plant Life</h3>
+        <button className="border border-gray-50 px-2 hover:bg-gray-50 hover:text-black transition-all duration-300 cursor-pointer">
+          Login
+        </button>
+      </header>
+    </div>
+  );
 };
